dc0528d49765bcb4e795cac1955f683a08ecc15b
Lucas Lopes <dev.thuram@gmail.com>
2022-02-02 09:15:35 -0300

    created animes template

--- src/templates/Animes/index.tsx
+++ src/templates/Animes/index.tsx
@@ -6,9 +6,9 @@ export const AnimesTemplate = () => {
   return (
     <Base>
       <Jumbotron
-        title="ANIMESSS"
-        subtitle="O Naped pode ser sua fonte de informações sobre o mundo nerd e outros
-      assuntos relacionados."
+        title="Animes"
+        subtitle="O Naped pode ser sua fonte de informações sobre o mundo nerd e outros assuntos relacionados."
+        backgroundURL="https://images.unsplash.com/photo-1578632749014-ca77efd052eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
       />
     </Base>
   )
