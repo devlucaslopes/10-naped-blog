d7814fb2b033f9d39b301ffd7998777d84118302
Lucas Lopes <dev.thuram@gmail.com>
2022-02-08 08:41:54 -0300

    fix cover url

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,6 +1,10 @@
 import { PrismicDocument } from '@prismicio/types'
 import { RichText } from 'prismic-dom'
 
+type Cover = {
+  url: string
+}
+
 export const dateFormatter = (date: string) => {
   return new Date(date).toLocaleDateString('pt-BR', {
     day: '2-digit',
@@ -13,11 +17,13 @@ export const fullPostFormatter = (document: PrismicDocument) => {
   const { first_publication_date, data } = document
   const { title, summary, content, cover } = data
 
+  const { url = '' } = cover as Cover
+
   return {
     title: RichText.asText(title),
     summary: RichText.asText(summary),
     content: RichText.asHtml(content),
-    cover: cover?.url,
+    cover: url,
     publicatedAt: dateFormatter(first_publication_date)
   }
 }
@@ -26,11 +32,13 @@ export const previewPostFormatter = (document: PrismicDocument) => {
   const { uid, tags, first_publication_date, data } = document
   const { title, summary, cover } = data
 
+  const { url = '' } = cover as Cover
+
   return {
     slug: `/post/${uid!}`,
     title: RichText.asText(title),
     summary: RichText.asText(summary),
-    cover: cover?.url,
+    cover: url,
     tag: tags[1],
     publicatedAt: dateFormatter(first_publication_date)
   }
